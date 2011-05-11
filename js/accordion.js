7413064555120c15e319c7a62c20b0ac3d662f8f
Richard Mortier <mort@cantab.net>
2011-05-11 14:19:44 +0100

    refresh: better front page icons

--- js/accordion.js
+++ js/accordion.js
@@ -3,11 +3,11 @@ $(document).ready(
         var lastBlock = $("ul#accordion a#default"),
             maxWidth = "500px",
             minWidth = "75px";	
-        var cnt=0;
         
         $("ul#accordion li a").hover(
             function() {
                 if (!($(lastBlock).get(0) === $(this).get(0))) {
+                    // ...test two elements for equality
                     $(lastBlock).animate({ width: minWidth }, { queue:false, duration:400 });
                     $(this).animate({ width: maxWidth }, { queue:false, duration:400 });
                     lastBlock = this;
